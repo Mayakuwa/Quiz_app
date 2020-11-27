7163673d50b56282776b2124b1acb89830b9b33f
Maya <melokitty.1003@gmail.com>
2020-11-27 17:47:19 +0900

    add new activity

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "please enter your name", Toast.LENGTH_SHORT).show()
             } else {
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
                 startActivity(intent)
                 finish()
             }
--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -16,7 +16,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition:Int = 1
     private var mQuestionList: ArrayList<Question>? = null
     private var mSelectedOptionPosition:Int = 0
-
+    private var mCorrectAnswers:Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,6 +74,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+
     override fun onClick(v: View?) {
         when(v?.id)  {
             R.id.tv_option_one -> {
@@ -106,6 +107,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                     val question  = mQuestionList?.get(mCurrentPosition -1)
                     if(question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswers++
                     }
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
 
